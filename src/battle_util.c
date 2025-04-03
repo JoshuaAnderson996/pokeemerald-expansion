dfb3dfb2cc33b86e7ea1213d3cef8e3f31b12dd2
JoshuaAnderson996 <130392593+JoshuaAnderson996@users.noreply.github.com>
2025-04-03 12:48:52 +0100

    Update battle_util.c

--- src/battle_util.c
+++ src/battle_util.c
@@ -9951,7 +9951,7 @@ static inline u32 CalcAttackStat(struct DamageCalculationData *damageCalcData, u
             modifier = uq4_12_multiply_half_down(modifier, UQ_4_12(2.0));
         break;
     case HOLD_EFFECT_LIGHT_BALL:
-        if (atkBaseSpeciesId == SPECIES_PIKACHU && (B_LIGHT_BALL_ATTACK_BOOST >= GEN_4 || IsBattleMoveSpecial(move)))
+    if ((atkBaseSpeciesId == SPECIES_PIKACHU || atkBaseSpeciesId == SPECIES_RAICHU || atkBaseSpeciesId == SPECIES_RAICHU_ALOLA || atkBaseSpeciesId == SPECIES_PLUSLE || atkBaseSpeciesId == SPECIES_MINUN || atkBaseSpeciesId == SPECIES_PACHIRISU || atkBaseSpeciesId == SPECIES_EMOLGA || atkBaseSpeciesId == SPECIES_DEDENNE || atkBaseSpeciesId == SPECIES_TOGEDEMARU || atkBaseSpeciesId == SPECIES_MORPEKO || atkBaseSpeciesId == SPECIES_PAWMOT) && (B_LIGHT_BALL_ATTACK_BOOST >= GEN_4 || IsBattleMoveSpecial(move)))
             modifier = uq4_12_multiply_half_down(modifier, UQ_4_12(2.0));
         break;
     case HOLD_EFFECT_CHOICE_BAND:
