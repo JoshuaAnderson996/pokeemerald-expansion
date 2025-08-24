cd95f9d6824b58ad8c21a2d4f019e876ca81b590
JoshuaAnder996 <josh1996ace@gmail.com>
2025-08-24 20:07:37 +0100

    Revert "Expand Rustboro Gym layout and add new trainers"
    This reverts commit d78fca0c4ef2307385ae7d60373b8747ad49f1bb.

--- include/constants/opponents.h
+++ include/constants/opponents.h
@@ -860,16 +860,12 @@
 #define TRAINER_MAY_PLACEHOLDER             854
 #define TRAINER_DICK                        855
 #define TRAINER_QUARTZ                      856
-#define TRAINER_MICA                        857
-#define TRAINER_OPAL                        858
-#define TRAINER_BASALT                      859
-#define TRAINER_GARNET                      860
 
 // NOTE: Because each Trainer uses a flag to determine when they are defeated, there is only space for 9 additional trainers before trainer flag space overflows
 //       More space can be made by shifting flags around in constants/flags.h or changing how trainer flags are handled
 //       MAX_TRAINERS_COUNT can be increased but will take up additional saveblock space
 
-#define TRAINERS_COUNT                      861
+#define TRAINERS_COUNT                      857
 #define MAX_TRAINERS_COUNT                  864
 #define TRAINER_PARTNER(partner)           (MAX_TRAINERS_COUNT + partner)
 
